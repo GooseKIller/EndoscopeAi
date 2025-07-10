09e87f8c8911da10f0eb88b8c9d314c23213067d
CaneRig <originalomail@gmail.com>
2025-07-10 12:59:50 +0300

    fix: rust backend init

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:endoscopy_ai/src/rust/frb_generated.dart';
 import 'package:flutter/material.dart';
 import 'package:fvp/fvp.dart' as fvp;
 
@@ -6,6 +7,7 @@ import 'app.dart';
 void main() async {
   fvp.registerWith(); // инициаллизация fvp
   WidgetsFlutterBinding.ensureInitialized(); // еще какаято инициализация
+  await RustLib.init();
 
   runApp(const App());
 }
