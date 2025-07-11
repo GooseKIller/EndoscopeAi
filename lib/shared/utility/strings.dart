d7bc37a9dca63d8da2018f8fdab787f5da6b2734
CaneRig <originalomail@gmail.com>
2025-07-11 21:16:47 +0300

    add: stringing the date

--- lib/shared/utility/strings.dart
+++ lib/shared/utility/strings.dart
@@ -13,3 +13,17 @@ String formatDuration(Duration duration) {
       ? '$hours:$minutes:$seconds'
       : '$minutes:$seconds';
 }
+
+// Форматировние даты в виде yyyy-mm-dd-hh-mm
+String formatDateTillMinDash(DateTime date) {
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
+  String fourDigits(int n) => n.toString().padLeft(4, '0');
+
+  String y = fourDigits(date.year);
+  String m = twoDigits(date.month);
+  String d = twoDigits(date.day);
+  String h = twoDigits(date.hour);
+  String min = twoDigits(date.minute);
+
+  return "$y-$m-$d-$h-$min";
+}
